dbf9702b7e2ed3befab2044a896eb439eb069b3a
Prasad0224 <gharoteprasad@gmail.com>
2026-08-27 07:31:42 +0530

    Add: basic Spring Boot application skeleton (health check endpoint)

---
+++ src/main/java/com/platform/DigitalAssetApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAssetApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalAssetApprovalApplication.class, args);
+    }
+
+}
